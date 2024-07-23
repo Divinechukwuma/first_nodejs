6802c672a1e6c21f72a7c7ae2b587956ee98fa8d
divinechukwuma <divinechukwuma982@gmail.com>
2024-07-23 20:05:23 +0100

    ok time to it be right back

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandlers = require('./middleware/errorhandlers');
 const app = express();
 const PORT = process.env.port || 3500;
+
+//log events function 
+app.use(logger)
+
+//cross origin resource sharing 
+// white list is a list of accepted domains that cors will have access to the backend data and after development make sure u remove it and the development domains 
+
+const whitelist = ['http://www.divinerules.com', 'http://127.0.01:5500', 'http://localhost:3500',];
+const corsOptions = {
+    origin: (origin, callback) => {
+        //during development add the no origin so u can excape the cores error of being undefinedand and  after development make sure u remove it and the development domains 
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            //THE FIRST SECTION ALWAYS NORMALLY STAND SFOR THE RROR SO THIS THIS IS SAYING IS IF THERE IS NO ERROR THEN THE FUNCTION IS TURE SO IT SHOULD RUN 
+            callback(null, true);
+        } else {
+            callback(new Error('not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 //built in middleware to handle urlencoded data
 // in other words, form data:
@@ -16,6 +39,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(logger)
+
 
 app.get('^/$|index(.html)?', (req, res) => {
     //res.sendFile('./view/index.html',(root: __dirname)); 
@@ -51,11 +75,23 @@ const three = (req, res, next) => {
     res.send('finished!');
 }
 
+//app.use is mostly used for middleware and app.ll is mostly used for routing it means it will ba applied to all http methods or requests
+
+
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status('404');
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 not found" });
+    } else {
+        req.type('txt').send('404 not found');
+    }
+
 })
 
+app.use(errorHandlers);
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));

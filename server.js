f328714cd0d0447aee54a281fb60e6ddd3216696
divinechukwuma <divinechukwuma982@gmail.com>
2024-08-01 13:38:46 +0100

    routing and api is quite interesting

--- server.js
+++ server.js
@@ -39,46 +39,10 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
+app.use('/', require('./routes/root'))
 app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('^/$|index(.html)?', (req, res) => {
-    //res.sendFile('./view/index.html',(root: __dirname)); 
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html');
-})
-
-//ROUTE HANDLERS AND CHAINS FUNCTIONS 
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html')
-    next()
-}, (req, res) => {
-    res.send('hello world!')
-})
-
-//another way to chain functions chaning in an array 
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-const three = (req, res, next) => {
-    console.log('three');
-    res.send('finished!');
-}
-
-//app.use is mostly used for middleware and app.ll is mostly used for routing it means it will ba applied to all http methods or requests
-
-
-app.get('/chain(.html)?', [one, two, three])
 
 app.all('*', (req, res) => {
     res.status('404');
